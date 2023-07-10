366a6d1ed7472deea72d7decd6085e7f252ad2d2
Lu <jlu.fernandez90@gmail.com>
2023-07-10 18:21:33 +0200

    iteration 2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,26 @@
 //  Add your code here
+const mongoose = require('mongoose')
+
+const celebritieSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Required field'],
+        },
+        ocupation: {
+            type: String,
+            required: [true, 'Required field'],
+        },
+        catchPhrase: {
+            type: String,
+            required: [true, 'Required field'],
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Celebrity = mongoose.model('Celebrity', celebritieSchema);
+
+module.exports = Celebrity
